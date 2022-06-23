e0549bcd140ec1a7cb493dffa5fdbcf8f7337416
Angela Robinette~ <angela@dev-angela.com>
2022-06-23 00:11:27 -0700

    Adds logic to the teams arrays within while loop and adds a break if member is falsey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   members: string[] = [];
   errorMessage ='';
   numberOfTeams: number | '' = '';
+  teams: string[][] = []
 
   onInput(member: string) {
     this.newMemberName = member;
@@ -28,6 +29,31 @@ export class AppComponent {
     this.errorMessage = '';
     this.members.push(this.newMemberName);
     this.newMemberName = '';
+  }
+
+  generateTeams() {
+
+    if(!this.numberOfTeams || this.numberOfTeams <= 0) {
+      return
+    }
+
+    const allMembers = [...this.members]
+
+   while(allMembers.length) {
+    for(let i = 0; i < this.numberOfTeams; i++) {
+      const randomIndex = Math.floor(Math.random() * allMembers.length )
+      const member = allMembers.splice(randomIndex, 1) [0];
+      if(!member)break;
+      if(this.teams[i]) {
+        this.teams[i].push(member)
+      } else {
+        this.teams[i] = [member]
+      }
+    }
+   }
+
+    console.log(this.teams);
+
   }
 
 }
