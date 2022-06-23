982a4e44bf41c9f52f974fb3833b6aba8e949782
Angela Robinette~ <angela@dev-angela.com>
2022-06-23 00:14:49 -0700

    Places content back to an empty state after a team is generated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,9 @@ export class AppComponent {
     for(let i = 0; i < this.numberOfTeams; i++) {
       const randomIndex = Math.floor(Math.random() * allMembers.length )
       const member = allMembers.splice(randomIndex, 1) [0];
+      
       if(!member)break;
+      
       if(this.teams[i]) {
         this.teams[i].push(member)
       } else {
@@ -52,7 +54,8 @@ export class AppComponent {
     }
    }
 
-    console.log(this.teams);
+    this.members = [];
+    this.numberOfTeams = '';
 
   }
 
